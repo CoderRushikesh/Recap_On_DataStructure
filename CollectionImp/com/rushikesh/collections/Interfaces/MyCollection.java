6448f7242454fa9c44847451b58231567d8e5abe
CoderRushikesh <rushikamble462@gmail.com>
2025-11-25 01:24:59 +0530

    Update MyCollection.java

--- CollectionImp/com/rushikesh/collections/Interfaces/MyCollection.java
+++ CollectionImp/com/rushikesh/collections/Interfaces/MyCollection.java
@@ -2,4 +2,24 @@ package CollectionImp.com.rushikesh.collections.Interfaces;
 
 public interface MyCollection<E> extends Iterator<E>{
     
+    boolean add(E element);
+
+    // Removes an element from the collection
+    boolean remove(E element);
+
+    // Checks if the collection contains a given element
+    boolean contains(E element);
+
+    // Returns the number of elements in the collection
+    int size();
+
+    // Checks if the collection is empty
+    boolean isEmpty();
+
+    // Clears all elements from the collection
+    void clear();
+
+    // Returns an iterator to traverse the collection
+    java.util.Iterator<E> iterator();
+
 }
